d5e46b15e308e27b2a0fe519cf3821a35f669c45
Gov2007 <73535096+Gov2007@users.noreply.github.com>
2020-11-14 16:42:14 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+class box {
+   
+constructor(x,y,width,height) {
+  var option = {
+    'restitution' : 0.8,
+    'friction' : 1.0,
+    'density' : 1.0
+  }
+   this.body = Bodies.rectangle(x,y,width,height,option);
+   this.w = width;
+   this.h = height;
+   World.add(world,this.body);
+   
+ }
+  display() {
+   var pos = this.body.position;
+   var angle = this.body.angle;
+   push();
+   translate(pos.x,pos.y);
+   rotate(angle);
+   rectMode(CENTER);
+   fill("green");
+   rect(0,0,this.w,this.h);
+   pop()
+  }
+}
